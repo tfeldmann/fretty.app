0b4ca4e8cedaf6aeeeca5f6ff2eed202bd9ca750
Thomas Feldmann <mail@tfeldmann.de>
2020-06-07 13:37:42 +0200

    init vue project

---
+++ fretty/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Fretboard tuning="[60,64,67]" notes="[60,64,67]" frets="15" />
+    <br>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Fretboard from "./components/Fretboard.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Fretboard,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ fretty/src/components/Fretboard.vue
@@ -0,0 +1,130 @@
+<template>
+  <svg width="800" height="300">
+    <template v-for="string in tuning.slice().reverse()">
+      <svg x="50" :y="50 + i_str * string_spacing" overflow="auto">
+        <line
+          x1="0"
+          y1="0"
+          :x2="fretpos(notes.length - 1)"
+          y2="0"
+          stroke="#000"
+          stroke-width="1"
+        />
+        <template v-for="(note, i_frt) in notes" :key="note.id">
+          <line
+            v-if="i_str == 0"
+            :x1="fretpos(i_frt)"
+            y1="0"
+            :x2="fretpos(i_frt)"
+            :y2="(display.length - 1) * string_spacing"
+            stroke-width="1"
+            stroke="#000"
+          />
+          <transition name="fade">
+            <svg
+              v-if="note"
+              :x="(fretpos(i_frt - 1) + fretpos(i_frt)) / 2"
+              y="0"
+              overflow="auto"
+            >
+              <circle
+                cx="0"
+                cy="0"
+                r="10"
+                stroke-width="1"
+                fill="white"
+                stroke="black"
+              />
+              <text
+                font-size="11"
+                x="0"
+                y="0"
+                alignment-baseline="middle"
+                fill="black"
+                text-anchor="middle"
+              >
+                {{ note }}
+              </text>
+            </svg>
+          </transition>
+        </template>
+      </svg>
+    </template>
+  </svg>
+</template>
+
+<script>
+import { Scale, Note, Range, Midi } from "@tonaljs/tonal";
+
+const string_spacing = 30;
+
+export default {
+  name: "Fretboard",
+  props: {
+    tuning: {
+      default: [],
+    },
+    notes: {
+      default: [],
+    },
+    frets: {
+      default: 18,
+    },
+    prefer_sharps: {
+      default: true, // TODO: "sharps", "flats" or "interval"
+    },
+  },
+  // computed: {
+  //   display() {
+  //     let result = [];
+  //     var scale_notes = this.scale_info.notes.map(Note.chroma);
+  //     for (var base of this.strings) {
+  //       var chromatic = Range.numeric([base, base + this.frets]);
+  //       var tmp = [];
+  //       for (var c of chromatic) {
+  //         if (scale_notes.includes(c % 12)) {
+  //           tmp.push({id: c, this.toname(c)});
+  //         } else {
+  //           tmp.push({id: c, null);
+  //         }
+  //       }
+  //       result.push(tmp);
+  //     }
+  //     return result;
+  //   },
+  // },
+  methods: {
+
+    fretpos(n) {
+      // https://www.liutaiomottola.com/formulae/fret.htm
+      const s = 1000;
+      var d = s - s / Math.pow(2, n / 12);
+      return Math.round(d * 1000) / 1000;
+    },
+    toname(x) {
+      return Midi.midiToNoteName(x, {
+        sharps: this.sharps,
+        pitchClass: true,
+      });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
